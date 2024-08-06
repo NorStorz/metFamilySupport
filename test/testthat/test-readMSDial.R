d4b72282b17bbadf811fcca1e48444c6d87e1216
Norman Storz <norman.storz@student.uni-halle.de>
2024-08-06 16:40:55 +0200

    tests for read functions

--- test/testthat/test-readMSDial.R
+++ test/testthat/test-readMSDial.R
@@ -2,41 +2,25 @@ library(QFeatures)
 source("R/readMsdial.R") 
 
 qf <- readMSDial("data/Metabolite_profile_showcase.txt")
-load("data/processMS1data.Rdata")
-counts <- sapply(18:23,function(i) as.numeric(metaboliteProfile[,i]))
 
 
 test_that("Sum of Intensities is correct", {
-  count_cols <- grep("TRI|LVS", names(metaboliteProfile))
-  counts <- sapply(count_cols,function(i) as.numeric(metaboliteProfile[,i]))
   
-  sumQfeature <- sum(colSums(assay(qf)))
-  sumMsdial <- sum(counts)
-  expect_equal(sumQfeature, sumMsdial)
+  sumQf <- sum(colSums(assay(qf)))
+  expect_equal(sumQf, 232301678)
 
 })
 
-test_that("Rows and Columns are correct", {
+test_that("Number of Rows and Columns are correct", {
   
   # check if number of rows is identical
-  nrowQfeature <- nrow(assay(qf))
-  nrowMsdial <- nrow(metaboliteProfile)
-  expect_equal(nrowQfeature, nrowMsdial)
+  nrowQf <- nrow(assay(qf))
+  expect_equal(nrowQf, 5823L)
   
   # check if number of cols is identical
-  ncolQfeature <- ncol(assay(qf))+ ncols(rowData(qf))
-  ncolMsdial <- ncol(metaboliteProfile)
-  expect_equal(as.integer(ncolQfeature), ncolMsdial)
+  ncolQf <- ncol(assay(qf))+ ncols(rowData(qf))
+  expect_equal(as.integer(ncolQf), 20)
   
-  #check if colnames match
-  colNamesQfeature <- c(colnames(rowData(qf))[[1]], rownames(colData(qf)))
-  colNamesMsdial <- colnames(metaboliteProfile)
-  
-  l1 <- length(colNamesMsdial[!(colNamesMsdial %in% colNamesQfeature)])
-  l2 <- length(colNamesQfeature[!(colNamesQfeature %in% colNamesMsdial)])
-  
-  expect_equal(l1, l2)
-  expect_equal(l2, 0)
 })
 
 
---
+++ test/testthat/test-readMetaboScape.R
@@ -0,0 +1,25 @@
+library(QFeatures)
+source("R/readMetaboScape.R") 
+
+
+qf <- readMetaboscape("data/rye_test_data.xlsx")
+
+test_that("Sum of Intensities is correct", {
+  sumQf <- sum(colSums(assay(qf)))
+  expect_equal(sumQf,307716661)
+  
+})
+
+test_that("Number of Rows and Columns are correct", {
+  
+  # check if number of rows is identical
+  nrowQf <- nrow(assay(qf))
+  expect_equal(nrowQf, 805L)
+  
+  # check if number of cols is identical
+  ncolQf <- ncol(assay(qf))+ ncols(rowData(qf))
+  expect_equal(as.integer(ncolQf), 73)
+  
+})
+
+
